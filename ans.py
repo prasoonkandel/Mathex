6c19d7bb2d0a7d0e7903276fb5dda102df70f027
Prasoon Kandel <prasoonkandel1029@gmail.com>
2025-12-15 15:08:52 +0545

    Adding Demo Question

--- ans.py
+++ ans.py
@@ -1,6 +1,13 @@
 import webbrowser
 from back import answer  
 
+# ==== demo question for testing ==== # (it was in my book XD)
+
+"""
+If (-1, 3), (1.-1) and (5,1) are the vertices of a triangle, find the the length of the median drawn from the first vertex.
+
+"""
+
 def mathex():
 
     print(r"""
@@ -10,6 +17,7 @@ def mathex():
  | |\/| |/ _` | __| '_ \ / _ \ \/ /   / /\ \   | |
  | |  | | (_| | |_| | | |  __/>  <   / ____ \ _| |_
  |_|  |_|\__,_|\__|_| |_|\___/_/\_\ /_/    \_\_____|
+          
 """)
 
     print("Type something (type 'bye' to exit)\n")
